d474b79ed6bbc4b713c5bedfd088b7dc9a34c2d5
Joseph Miller <josephclaymiller@gmail.com>
2018-05-05 13:23:20 -0700

    Initial Commit

---
+++ Sound Picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sound Picker
+//
+//  Created by Joseph Miller on 5/5/18.
+//  Copyright © 2018 Joseph Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
